77f71901792fdbc860714d7f2ccf93d5cf038bc6
adnil-codes <pwitylinda@gmail.com>
2022-04-07 19:49:29 -0700

    task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,11 @@
 #include <unistd.h>
 
 /**
- * main- Entry point
- *
- * return: Always 0 (Success)
+ * main - Function begins
+ * Return: Function ends
  */
 int main(void)
 {
-write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-return (1);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return(1);
 }
