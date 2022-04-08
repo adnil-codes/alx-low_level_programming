8fb46335ceb648b8f887eb1b0ee6fa37ee2de98d
adnil-codes <pwitylinda@gmail.com>
2022-04-08 09:26:11 -0700

    Task 4

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - task
+ *
+ * Return: 0 when codes ru
+ */
+int main(void)
+{
+char ch = 97;
+for (ch = 97; ch < 122; ch++)
+{
+putchar(ch);
+}
+char up = 65;
+for up = 65; up < 90; up++)
+{
+putchar(up);
+}
+
+return (0);
+}
+
