535e0d9b6c4e8940c6182b3b2aa73505120053d7
adnil-codes <pwitylinda@gmail.com>
2022-04-12 09:05:28 -0700

    Prints the alphabets in lower case

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *print_alphabet - prints
+ *
+ *Description: this code prints lower case alphabets
+ *Return: 0 when sucess
+ */
+void print_alphabet(void)
+{
+int i = 97;
+{
+while (i <= 122)
+{
+_putchar(i);
+i++;
+}
+}
+}
