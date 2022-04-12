8044459fff18dec3bb64b232a007a41a6aa5438c
adnil-codes <pwitylinda@gmail.com>
2022-04-12 08:18:57 -0700

    Prints the alphabets in lower case for 10 new lines

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - code input
+ * Description: Code prints the alphabets
+ * in lower case 10 times in new lins
+ * Result: Returns 0 when sucess
+ */
+int main(void)
+{
+int i, j;
+for(i = 0; i < 10; i++)
+{
+for(j = 97; j < 122; j++)
+{
+putchar(j);
+}
+putchar('\n');
+}
+}
