36303f3d29f4f1cc89d771606cd091c73a84eccc
adnil-codes <pwitylinda@gmail.com>
2022-04-12 14:21:52 -0700

    prints sign of a number

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_sign - prints time
+ * Description: Code prints return depending on outcome
+ * Return: 0, -1 or 1 depending on outcome
+ */
+int print_sign(int n)
+{
+if (n >= 0)
+{
+return (1);
+{
+_putchar('+');
+}
+else if (n == 0)
+{
+return (0);
+}
+{
+_putchar('0');
+}
+else
+{
+return (-1);
+}
+{
+_putchar('-');
+}
+}
+}
