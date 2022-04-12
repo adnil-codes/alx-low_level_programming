7237c5500575fb4b26205d86e5983c7ba449876e
adnil-codes <pwitylinda@gmail.com>
2022-04-12 08:25:51 -0700

    Prints the alphabets in lower case for 10 new lines

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,12 +9,13 @@
 int main(void)
 {
 int i, j;
-for(i = 0; i < 10; i++)
+for (i = 0; i < 10; i++)
 {
-for(j = 97; j < 122; j++)
+for (j = 97; j < 122; j++)
 {
 putchar(j);
 }
 putchar('\n');
 }
+return (0);
 }
