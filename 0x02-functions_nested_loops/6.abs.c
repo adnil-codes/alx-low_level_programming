cafd7c66778d099297b9bc1a092ffdc048ffab71
adnil-codes <pwitylinda@gmail.com>
2022-04-12 16:35:02 -0700

    Print absolute value of integer

---
+++ 0x02-functions_nested_loops/6.abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _abs - absolute
+ * @r: integer of code
+ * Description: This should print the absolute value
+ * Return: always 0
+ */
+int _abs(int r)
+{
+r = abs(r);
+print("%d", r);
+return (0);
+}
