ca8eeea4720cb709edb756ad6ed65b52c3049bb0
adnil-codes <pwitylinda@gmail.com>
2022-04-12 09:17:44 -0700

    Prints putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,28 +1,24 @@
 #include <stdio.h>
+#include "main.h"
 
 /**
  * main - main code
  * Description: This code prints putchar
  * \\s: the s[] member is used to define the character
  * of the function we want to print using putchar
- * Result: Always 0 if succesful
+ * Return: Always 0 if succesful
  */
-void print_putchar(char *s);
 int main(void)
 {
-char s[] = "_putchar";
-print_putchar(s);
-putchar('\n');
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
 
 return (0);
-}
-
-void print_putchar(char *s);
-{
-int i = 0;
-while (s[i] != '\n' && s[i] != '\0')
-{
-putchar(s[i]);
-i++;
-}
 }
