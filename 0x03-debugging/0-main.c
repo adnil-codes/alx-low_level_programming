4217f44273886a580d5c1b44ddcd90f9eb46102b
adnil-codes <pwitylinda@gmail.com>
2022-04-14 06:11:24 -0700

    Prints is an integer is positive or negative

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,14 +1,17 @@
 #include "main.h"
 
 /**
-* main - main code
-* Desciption: function prints if integer is positive or negative
-* Return: 0 if success
+* main - tests function that prints if integer is positive or negative
+* Return: 0
 */
+
 int main(void)
 {
-int i;
-i = 0;
-positive_or_negative(i)
-return (0);
+
+	int i;
+
+	i = 0;
+	positive_or_negative(i)
+
+	return (0);
 }
