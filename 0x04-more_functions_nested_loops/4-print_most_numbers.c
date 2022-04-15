4ce385bf27a0d78e04554bd1f6a0eabd37c22361
adnil-codes <pwitylinda@gmail.com>
2022-04-15 08:33:11 -0700

    print numbers between 0 and 9 except 2 and 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - Print some numbers
+ * Return:0
+ */
+void print_most_numbers(void)
+{
+	int i:
+
+	for (i = 48; i < 58; i++)
+	{
+		if (i != 50 && i != 52)
+		{
+			_putchar(i);
+		}
+	}
+	_putchar('\n');
+}
