d728e436b5ceadcb347fe0ea25d242813337ab7e
adnil-codes <pwitylinda@gmail.com>
2022-04-15 09:01:08 -0700

    Prints 10 times the number

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints numbers
+ * Return: Always 0
+ */
+void more_numbers(void)
+{
+	int i, j;
+
+	i = 0
+
+	while (i < 10)
+	{
+		for (j = 0; j < 15; j++)
+		{
+			if (j >= 10)
+			{
+				_putchar((j / 10) + 48);
+			}
+			_putchar((j % 10) + 48);
+		}
+		_putchar('\n');
+	}
+}
