280ba102df2fec4559950a4aad9586e0dc7ae272
adnil-codes <pwitylinda@gmail.com>
2022-04-15 07:42:21 -0700

    print numbers between 0 and 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - print numbers 0 to 9
+ * Return: values
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 48, i <= 58; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
