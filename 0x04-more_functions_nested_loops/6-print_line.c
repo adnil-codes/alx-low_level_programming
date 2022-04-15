7fae31ec8548fe84c2c397dba832fde93cf6f99a
adnil-codes <pwitylinda@gmail.com>
2022-04-15 12:16:08 -0700

    Print straight lines

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_line - prints straight line
+ * @n: parameter
+ * Return: straight line
+ */
+void print_line(int n)
+{
+	int i = 1;
+	(while i <= n)
+	{
+		_putchar(95);
+		i++;
+	}
+	_putchar('\n');
+}
