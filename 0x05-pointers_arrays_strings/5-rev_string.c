8227c9a4d1d64559250ee0d1a03f1ebb52a1607c
adnil-codes <pwitylinda@gmail.com>
2022-04-20 08:06:22 -0700

    print string in reverse

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * rev_string - function that reverses a string
+ * @s: pointer to an int that will be reversed
+ * Return: 0 when succesful
+ */
+void rev_string(char *s)
+{
+	char *start_c, *end_c, c;
+	int i, count;
+	int length = 0;
+
+	for (i = 0; s[i]; i++)
+	{
+		length++;
+	}
+	count = length;
+
+	start_c = s;
+	end_c = s;
+
+	for (i = 0; i < count - 1; i++)
+	{
+		end_c++;
+	}
+
+	for (i = 0; i < count / 2; i++)
+	{
+		c = *end_c;
+		*end_c = *start_c;
+		*start_c = c;
+
+		start_c++;
+		end_c--;
+	}
+}
