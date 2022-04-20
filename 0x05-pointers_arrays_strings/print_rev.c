47eaab0c99010c786a85998a57dd408741f39d39
adnil-codes <pwitylinda@gmail.com>
2022-04-20 07:30:19 -0700

    print string in reverse followed by new line

---
+++ 0x05-pointers_arrays_strings/print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_rev - Prints string in reverse
+ * @s: pointer to integer that will be chnaged
+ * Return: Void when succesful
+ */
+void print_rev(char *s)
+{
+	int i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	for(i = i - 1; i >= 0; i--)
+	{
+		_putchar (s[i]);
+	}
+	_putchar('\n');
+}
