c08ecfe975ad2b1ca339ba995c22cd91fb1d0a96
adnil-codes <pwitylinda@gmail.com>
2022-04-20 04:01:09 -0700

    Function that updates a pointer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - this updates the integer value to 98
+ * @n: pointer to the interger to be updated
+ * Return: Always 0 if succesful
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+README.md file for pointers, array and string
