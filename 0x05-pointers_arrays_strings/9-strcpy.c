e4078e47cb98072d8b71b2fc2c61ed7350050094
adnil-codes <pwitylinda@gmail.com>
2022-04-20 08:54:30 -0700

    cpoies a string pointed to by a src

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -3,11 +3,13 @@
 /**
  * _strcpy - copies string pointed to by src
  * @src: pointer to char to be copied
+ * @dest: pointer to char to be copied
  * Return: dest
  */
 char *_strcpy(char *dest, char *src)
 {
 	int i;
+
 	i = 0;
 
 	while (src[i] != '\0')
