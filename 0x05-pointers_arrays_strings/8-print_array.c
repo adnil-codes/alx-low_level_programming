772e42c9c9e19519de3b0febc96d729b499d6425
adnil-codes <pwitylinda@gmail.com>
2022-04-20 08:41:00 -0700

    print an array of integers using printf

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - prints n element of an array
+ * @a: pointer to an integer
+ * @n: value to be returned
+ * Return: Void
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	i = 0;
+	while (i < n)
+	{
+		printf("%d", a[i]);
+
+		if (i < n - 1)
+		{
+			printf(", ");
+		}
+		i++
+	}
+
+	printf("\n");
+	}
+}
