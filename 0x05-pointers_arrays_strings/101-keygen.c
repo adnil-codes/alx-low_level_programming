f6c81219f1bc35379e8679614ba3fbb2fa86839d
adnil-codes <pwitylinda@gmail.com>
2022-04-20 13:51:31 -0700

    generates random password

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -10,10 +10,18 @@
 
 int main(void)
 {
-	int num;
+int r = 0, c = 0;
+time_t t;
 
-	srand(time(0));
-	num = rand();
-	printf("%i\n", num);
-	return (0);
+srand((unsigned int) time (&t));
+while (c < 2772)
+{
+	r = rand() % 128;
+	if ((c + r) > 2772)
+		break;
+	c = c + r;
+	printf("%c", r);
+}
+printf("%c\n", (2772 - c));
+return (0);
 }
