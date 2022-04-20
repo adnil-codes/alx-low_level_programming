067b10a7141c06989759d0c3e14d97270a8af5af
adnil-codes <pwitylinda@gmail.com>
2022-04-20 06:07:10 -0700

    prints the length of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdlib>
+
+/**
+ * _strlen - function that prints length of string
+ * @s: pointer of the integer to be updated
+ * Return: Void if succesful
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	i = 0;
+	(while s[u] != '\0')
+	{
+		u++;
+	}
+	return (u);
+}
