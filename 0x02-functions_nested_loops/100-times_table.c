b0ef9744c95d41daa3edec7b94715f88f53b45c2
adnil-codes <pwitylinda@gmail.com>
2022-04-21 06:26:15 -0700

    print n-times table starting with 0

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,49 +1,49 @@
 #include "main.h"
 
 /**
- * print_times_table - print multiplication table
- * @n: paramenter
- * Description: Code prints the multiplication table of values
- * Return: multiplication table of integer
+ *  print_times_table - print multiplication table
+ *  @n: parameter
  */
 void print_times_table(int n)
 {
-int i, j, k;
-if (n >= 0 && n < 15)
-{
-for (i = 0; i <= n; i++)
-{
-for (j = 0; j <= n; j++)
-{
-k = i * j;
-if (j == 0)
-_putchar('0');
-else if (k < 10)
-{
-_putchar(',');
-_putchar(' ');
-_putchar(' ');
-_putchar(' ');
-_putchar(k % 10 + '0');
-}
-else if (k >= 10 && k < 100)
-{
-_putchar(',');
-_putchar(' ');
-_putchar(' ');
-_putchar((k / 10) % 10 + '0');
-_putchar(k % 10 + '0');
-}
-else if (k > 99 && k < 1000)
-{
-_putchar(',');
-_putchar(' ');
-_putchar(k / 100 + '0');
-_putchar((k / 10) % 10 + '0');
-_putchar(k % 10 + '0');
-}
-}
-_putchar('\n');
-}
-}
+	int i, j, k;
+
+	if (n >= 0 && n < 15)
+	{
+		for (i = 0; i <= n; i++)
+		{
+			for (j = 0; j <= n; j++)
+			{
+				k = i * j;
+
+				if (j == 0)
+					_putchar('0);
+				else if (k < 10)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(k % 10 + '0');
+				}
+				else if (k >= 10 && k < 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((k / 10) % 10 + '0');
+					_putchar(k % 10 + '0');
+				}
+				else if (k > 99 && k < 1000)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(k /100 + '0');
+					_putchar((k / 10) % 10 + '0');
+					_putchar(k % 10 + '0');
+				}
+			}
+			_putchar('\n');
+		}
+	}
 }
