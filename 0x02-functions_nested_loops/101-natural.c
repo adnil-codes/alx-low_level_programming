19fb698b6d23eaa0b21bbab0b483b1ce4453f875
adnil-codes <pwitylinda@gmail.com>
2022-04-21 06:42:48 -0700

    print natural numbers

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -8,18 +8,20 @@ int main(void)
 {
 unsigned long int sum3, sum5, sum;
 int i;
+
 sum3 = 0;
 sum5 = 0;
 sum = 0;
+
 for (i = 0; i < 1024; ++i)
 {
 if ((i % 3) == 0)
 {
-sum3 = sum3 + 1;
+sum3 = sum3 + i;
 }
 else if ((i % 5) == 0)
 {
-sum5 = sum5 + 1;
+sum5 = sum5 + i;
 }
 }
 sum = sum3 + sum5;
