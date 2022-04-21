ec03f6c8d6f8f271ad672c7b30bf26b123a3baf9
adnil-codes <pwitylinda@gmail.com>
2022-04-21 06:39:54 -0700

    print n-times table starting with 0

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,8 +1,9 @@
 #include "main.h"
 
 /**
- *  times_table - print multiplication table
+ * print_times_table - print multiplication table
  *  @n: parameter
+ *  Return: 0
  */
 void print_times_table(int n)
 {
@@ -38,7 +39,7 @@ void print_times_table(int n)
 				{
 					_putchar(',');
 					_putchar(' ');
-					_putchar(k /100 + '0');
+					_putchar(k / 100 + '0');
 					_putchar((k / 10) % 10 + '0');
 					_putchar(k % 10 + '0');
 				}
