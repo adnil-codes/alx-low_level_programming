cc15dac99eb924f6c5a01d833f0f9e76c950c817
adnil-codes <pwitylinda@gmail.com>
2022-04-22 05:24:41 -0700

    print integers

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * print_number - prints an integer
+ * @n: input integer
+ * Return: void
+ */
+void print_number(int n)
+{
+	unsigne int i, j, count;
+
+	if (n < 0)
+	{
+		_putchar(45);
+		i = n * -1;
+	}
+	else
+	{
+		i = n;
+	}
+
+	j = i;
+	count = 1;
+
+	while (j > 9)
+	{
+		j /= 10;
+		count *= 10;
+	}
+
+	for (; count >= 1; count /= 10)
+	{
+		_putchar(((m / count) % 10) + 48);
+	}
+
+}
