48ebeefe4b8a73c930d0f9ab30fac7a22b64f01e
adnil-codes <pwitylinda@gmail.com>
2022-04-23 23:40:27 -0700

    Reverse contnent of array

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -14,10 +14,9 @@ void reverse_array(int *a, int n)
 	{
 		for (j = i + 1; j > 0; j--)
 		{
-			tmp = a[j];
-			a[j] = a[j - 1];
+			tmp = *(a + j);
+			*(a + j) = *(a + (j - 1));
 			*(a + (j - 1)) = tmp;
 		}
 	}
-	return (0);
 }
