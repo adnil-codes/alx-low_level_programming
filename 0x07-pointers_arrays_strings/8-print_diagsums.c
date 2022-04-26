f81ff4ccf1455de2cfeb580f74dd22efd368ea68
adnil-codes <pwitylinda@gmail.com>
2022-04-26 06:23:54 -0700

    prints the sum of intigers in diagonals

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -5,7 +5,7 @@
  * print_diagsums - prints the sum of the two diagonals
  * of a square matrix of integers
  * @a: input pointer
- * size:size
+ * @size: size
  * Return: void
  */
 void print_diagsums(int *a, int size)
