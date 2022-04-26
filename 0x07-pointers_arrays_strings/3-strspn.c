28b80e5ba04e71b3325f7f2330f519041ffe0e35
adnil-codes <pwitylinda@gmail.com>
2022-04-26 05:20:00 -0700

    gets length of prefix substring

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strspn - Gets the length of a prefix substring
+ * @s: string with substring to be accessed
+ * @accept: substr of accepter char
+ * Returm: length of occurence
+ */
+unsigned int_strspn(char *s, char *accept)
+{
+	unsigned int c = 0;
+	char *t = accept;
+
+	while (*s++)
+	{
+		while (*accept++)
+			if (*(s - 1) == *(accept - 1))
+			{
+				c++;
+				break;
+			}
+		if (!(*--accept))
+			break;
+	}
+	return (c);
+}
