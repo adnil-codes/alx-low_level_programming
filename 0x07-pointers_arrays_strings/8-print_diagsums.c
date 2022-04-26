94100c76cd7ff48454ccf9518b92d9a4dd2cbbd8
adnil-codes <pwitylinda@gmail.com>
2022-04-26 06:21:56 -0700

    prints the sum of intigers in diagonals

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * print_diagsums - prints the sum of the two diagonals 
+ * print_diagsums - prints the sum of the two diagonals
  * of a square matrix of integers
  * @a: input pointer
  * size:size
@@ -15,7 +15,7 @@ void print_diagsums(int *a, int size)
 	for (i = 0; i < size; i++)
 	{
 		sum1 += *(a + (size * i + i));
-		sum2 += *(a + (size * i + size - 1 -i));
+		sum2 += *(a + (size * i + size - 1 - i));
 	}
 	printf("%d, ", sum1);
 	printf("%d\n", sum2);
