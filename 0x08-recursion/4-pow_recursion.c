4e903ee38290a1cb5c3dfd92925a9fa7c6e7bd87
adnil-codes <pwitylinda@gmail.com>
2022-04-28 06:22:54 -0700

    return val of x raised to power of y

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - prints the power of x to y
+ * @x: integer parameter
+ * @y: integer parameter
+ * Return:recursion
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	if (y == 0)
+	{
+		return (1);
+	}
+	return (x * _pow_recursion(x, y -1));
+}
