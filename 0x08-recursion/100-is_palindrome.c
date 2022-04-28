043fd38236fa8bff912a6cb8538982ba06dbc5fa
adnil-codes <pwitylinda@gmail.com>
2022-04-28 07:45:54 -0700

    checks if string is a palindrome

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,50 +1,47 @@
 #include "main.h"
 
 /**
- * is_palindrome - returns 1 if a string is a palindrome
+ * _strlen_recursion - size
  * @s: int parameter
- * Return: 1
- */
-int is_palindrome(char *s)
-{
-	int first, last;
-
-	first = 0;
-	last = _strlen_recursion(s) - 1;
-	return (palindrome(s, first, last));
-}
-
-/**
- * palindrome - entry point
- * @s: char parameter
- * @first: int parameter
- * @last: int parameter
- * Return: Recursion
- */
-int palindrome(char *s, int first, int last)
-{
-	if (first > last)
-	{
-		return (1);
-	}
-	else if (s[first] == s[last])
-	{
-		return (palindrome(s, first + 1, last - 1));
-	}
-	else
-		return (0);
-}
-
-/**
- * _strlen_recursion - entrt point
- * @s: char parameter
  * Return: 1
  */
 int _strlen_recursion(char *s)
 {
-	if (*s != '\0')
+	if (!*s)
 	{
-		return (1 + _strlen_recursion(s + 1));
+		return (0);
 	}
+	return (1 + _strlen_recursion(++s));
+}
+
+/**
+ * p1 - palindrome
+ * @s: char parameter
+ * @l: int parameter
+ * Return: Recursion
+ */
+int palindrome(char *s, int l)
+{
+	if (l < 1)
+	{
+		return (1);
+	}
+
+	if (*s == *(s + 1))
+	{
+		return (p1(s + 1, l - 2));
+	}
+	return (0);
+}
+
+/**
+ * is_palindrome - palindrome
+ * @s: pointer to string
+ * Return: 1
+ */
+int is_palindrome(char *s)
+{
+	int len = _strlen_recursion(s);
+
 	return (0);
 }
