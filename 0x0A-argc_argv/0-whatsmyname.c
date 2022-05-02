6e95ed15babb7bdfc8947addb039d1e203995b28
adnil-codes <pwitylinda@gmail.com>
2022-05-02 10:40:22 -0700

    prints name of file

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - the int main
+ * @argc: int parameter
+ * @argv: char parameter
+ * return: print the name
+ */
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
