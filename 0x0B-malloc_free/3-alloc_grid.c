ae7ec1697eb289362372f63a9b9dcc19f7802b11
linda chukwu <lindachukwu961@gmail.com>
2022-05-05 15:47:57 +0100

    returns pointer of 2-dim array

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -19,7 +19,7 @@ int **alloc_grid(int width, int height)
 	if (gridout == NULL)
 	{
 		free(gridout);
-		return(NULL);
+		return (NULL);
 	}
 
 	for (i = 0; i < height; i++)
@@ -33,7 +33,7 @@ int **alloc_grid(int width, int height)
 			return (NULL);
 		}
 	}
-	
+
 	for (i = 0; i < height; i++)
 		for (j = 0; j < width; j++)
 			gridout[i][j] = 0;
