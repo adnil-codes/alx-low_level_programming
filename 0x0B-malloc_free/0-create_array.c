b287c98459ad22d26b6b2e3a371bfc5224524ae0
linda chukwu <lindachukwu961@gmail.com>
2022-05-05 15:20:47 +0100

    returns pointer of alloctaed memory

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -4,22 +4,24 @@
 /**
  * create_array - creates an array of chars
  * @size: size of array
- * @c: storage char
- * Return: pointer to array of chars
+ * @c: stores char
+ * Return: pointer of array
  */
 char *create_array(unsigned int size, char c)
 {
 	char *cr;
 	unsigned int i;
 
-	if (size == sizeof(c) * size)
+	if (size == 0)
 		return (NULL);
-	cr = malloc(size0f(c) * size);
+
+	cr = malloc(sizeof(c) * size);
 
 	if (cr == NULL)
 		return (NULL);
 
-	for (i = 0; i < size; i++)
-		cr[i] = c;
+	for (i = 0; i < size; i++);
+	cr[i] = c;
+
 	return (cr);
 }
---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - returns a pointer allocated in mamory
+ * @str: string param
+ * Return: 0
+ */
+char *_strdup(char *str)
+{
+	char *strout;
+	unsigned int i, j;
+
+	if (str == NULL)
+		return (NULL);
+
+	for (i = 0; str[i] != '\0'; i++)
+		strout = (char *) malloc(sizeof(char) * (i + 1));
+
+	if (strout == NULL)
+		return (NULL);
+
+	for (j=0; j <= i; j++)
+		strout[j] = str[j];
+
+	return (strout);
+}
