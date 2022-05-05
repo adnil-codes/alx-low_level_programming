939a8ff57a9c6a4a83262db2793d8c76bc8aed24
linda chukwu <lindachukwu961@gmail.com>
2022-05-05 15:54:52 +0100

    frees a 2-dim grid

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -11,7 +11,7 @@ void free_grid(int **grid, int height)
 {
 	if (grid != NULL && height != 0)
 	{
-		for(; height >= 0; height--)
+		for (; height >= 0; height--)
 			free(grid[height]);
 		free(grid);
 	}
