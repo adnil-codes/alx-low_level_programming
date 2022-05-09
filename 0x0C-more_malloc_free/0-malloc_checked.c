6e7167e988a7956b3392597edb506f160ee537f5
linda chukwu <lindachukwu961@gmail.com>
2022-05-09 12:52:21 +0100

    concatenates two strings

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -11,6 +11,9 @@ void *malloc_checked(unsigned int b)
 
 	ptr = malloc(b);
 	if (ptr == NULL)
-		exit (98);
+	{
+		exit(98);
+	}
+
 	return (ptr);
 }
--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -40,7 +40,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		p[i] = s1[i];
 	}
 
-	for (; i < (size1 + n); i++)
+	for (i = 0; i < (size1 + n); i++)
 	{
 		p[i] = s2[i - size1];
 	}
