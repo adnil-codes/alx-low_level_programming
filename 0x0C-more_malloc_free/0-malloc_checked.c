d0bffa6fc6f9e1ddac93f38d48b346d8fd993774
linda chukwu <lindachukwu961@gmail.com>
2022-05-09 12:29:17 +0100

    allocates memory space using malloc

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * malloc_checked - allocates mem using malloc
+ * @b: bytes param
+ * Return: pointer to allocated mem
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+
+	ptr = malloc(b);
+	if (ptr == NULL)
+		exit(98);
+	return (ptr);
+}
