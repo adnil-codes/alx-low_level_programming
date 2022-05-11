2549a230db1f884590e2b5ebd1266b713e23839d
linda chukwu <lindachukwu961@gmail.com>
2022-05-11 10:39:18 +0100

    Prints name of file

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,4 +1,12 @@
+#include <stdio.h>
+
+/**
+ *main: Prints name of file the program
+ * was compiles from
+ *Return: 0
+ */
 int main(void)
 {
-printf("%s", _FILE_)
+	printf("%s\n", __FILE__);
+	return (0);
 }
