7d278a8213cfff20dd77efddc0029dc60c9adae9
linda chukwu <lindachukwu961@gmail.com>
2022-05-11 13:33:17 +0100

    Tasks on typedefs and structures

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,5 @@
-#ifndef DOG_H
-#define DOG_H
+#ifndef _DOG_H
+#define _DOG_H
 
 /**
  * struct dog - struct
@@ -15,7 +15,12 @@ struct dog
 	char *owner;
 };
 
+/**
+ * dot_t - Typedef for struct dog
+ */
 typedef struct dog dog_t;
+
+int _putchar(char);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
