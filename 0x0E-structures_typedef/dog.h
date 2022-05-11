1636ef1be47c54fbc1faecde7124a0bdff2618b8
linda chukwu <lindachukwu961@gmail.com>
2022-05-11 16:12:38 +0100

    typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -8,17 +8,14 @@
  * owner: owner parama
  * Description: Struct for dog
  */
-typedef struct dog
+struct dog
 {
 	char *name;
 	float *age;
 	char *owner;
-} dog_t;
+};
 
-/**
- * dot_t - Typedef for struct dog
- */
-
+typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
