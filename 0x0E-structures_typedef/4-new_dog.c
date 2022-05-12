64656f85e1393370bd87e4d39a68c8c0d715976f
linda chukwu <lindachukwu961@gmail.com>
2022-05-12 02:12:58 +0100

    prototype

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -10,26 +10,42 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *d;
-	d = malloc(sizeof(dog_t));
-	if (d == NULL)
-		return (NULL);
+	dog_t *p_dog;
+	int i, lname, lowner;
 
-	d->name = name;
-	if (d->name == NULL)
+	p_dog = malloc(sizeof(*p_dog));
+	if (p_dog == NULL || !(name) || !(owner))
 	{
-		free(d);
-		return (NULL);
-	}
-	d->age = age;
-	d->owner = owner;
-	if (d->owner == NULL)
-	{
-		free(d);
-		free(d->name);
+		free(p_dog);
 		return (NULL);
 	}
 
+	for (lname = 0; name[lname]; lname++)
+		;
 
-	return (d);
+	for (lowner = 0; owner[lowner]; lowner++)
+		;
+
+	p_dog->name = malloc(lname + 1);
+	p_dog->owner = malloc(lowner + 1);
+
+	if (!(p_dog->name) || !(p_dog->owner))
+	{
+		free(p_dog->owner);
+		free(p_dog->name);
+		free(p_dog);
+		return (NULL);
+	}
+
+	for (i = 0; i < lname; i++)
+		p_dog->name[i] = name[i];
+	p_dog->name[i] = '\0';
+
+	p_dog->age = age;
+
+	for (i = 0; i < lowner; i++)
+		p_dog->owner[i] = owner[i];
+	p_dog->owner[i] = '\0';
+
+	return (p_dog);
 }
