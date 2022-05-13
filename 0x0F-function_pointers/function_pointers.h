f094349ed3ccd823d516a24685a3b4da647c1ae9
adnil-codes <pwitylinda@gmail.com>
2022-05-13 06:40:11 -0700

    function pointers tasks

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,7 +1,7 @@
 #ifndef _FUCNTION_POINTERS_H_
 #define _FUNCTION_POINTERS_H_
 
-#include <stddef.h>
+#include <stdlib.h>
 
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
