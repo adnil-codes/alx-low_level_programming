8518f5dbc5179ce0b293acd1915927bc92087e8d
adnil-codes <pwitylinda@gmail.com>
2022-05-13 06:56:30 -0700

    function pointers tasks

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,3 @@
-#include <stdlib.h>
 #include "function_pointers.h"
 
 /**
@@ -10,6 +9,6 @@
 
 void print_name(char *name, void (*f)(char *))
 {
-	if (name != NULL && f != NULL)
+	if (name && f)
 		f(name);
 }
