db7045a0901bcb932c72df6534e0132e45c7addc
linda chukwu <lindachukwu961@gmail.com>
2022-05-16 16:22:28 +0100

    print numbers, strings and both using variadic arg

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -3,24 +3,27 @@
 /**
  * print_numbers - prints numbers
  * @separator: stirng to be printeed
- * @n: integers 
+ * @n: integers
+ * @...: Variable numbers to be printed
  * Return: void
  */
 void print_numbers(const char *searator, const unsigned int n, ...)
 {
-	va_list valist;
+	va_list num;
 	unsigned int i;
 
-	va_start(valist, n);
+	va_start(num, n);
 
 	for (i = 0; i < n; i++)
 	{
-		printf("%d", va_arg(valist, int));
-		if (separator && i < n - 1)
+		printf("%d", va_arg(num, int));
+
+		if (i != (n - 1) && separator != NULL)
 			printf("%s", separator);
 	}
 
 	printf("\n");
-	va_end(valist);
+
+	va_end(num);
 }
 
--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -4,30 +4,33 @@
  * print_strings - prints strings
  * @separator: strings to be prited
  * @n: integers param
+ * @...: variable number of strings to be printed
  * Return: void
+ * Description: If separator is null, it wont be printed
+ * 		if its NULL, nil is printed
  */
 void print_strings(const char *separator, const unsigned int n, ...)
 {
-	va_list valist;
+	va_list strings;
 	unsigned int i;
 	char *str;
 
-	va_start(valist, n)
+	va_start(strings, n)
 
 		for (i = 0; i < n; i++)
 		{
-			str = va_arg(valist, char *);
+			str = va_arg(strings, char *);
 
-			if (str)
-				printf("%s", str);
-			else
+			if (str == NULL)
 				printf("(nil)");
+			else
+				printf("%s", str);
 
-			if (i < n - 1)
-				if (separator)
-					printf("%s", separator);
+			if (i != (n - 1) && separator != NULL)
+				printf("%s", separator);
 		}
 
 	printf("\n");
-	va_end(valist);
+
+	va_end(strings);
 }
