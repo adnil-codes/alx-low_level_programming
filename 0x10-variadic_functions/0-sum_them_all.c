2449fd5fe0f6c6d57c9c84dfd13e308bc0a0964e
linda chukwu <lindachukwu961@gmail.com>
2022-05-16 16:00:27 +0100

    print numbers, strings and both using variadic function

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -21,6 +21,6 @@ int sum_them_all(const unsigned int n, ...)
 
 	va_end(valist);
 
-	return(sum);
+	return (sum);
 }
 
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,8 +1,15 @@
-#ifndef _VARIADIC_FUNCTIONS_
-#define _VARIADIC_FUNCTIONS_
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_
 
 #include <stdarg.h>
 #include <stdio.h>
+
+typedef struct printer
+{
+	char *symbol;
+	void (*print)(va_list arg);
+
+} printer_t;
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
