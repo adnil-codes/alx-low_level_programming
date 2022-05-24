8a896256b52d02fe1c0a3e3c6ad01906fce06e93
linda chukwu <lindachukwu961@gmail.com>
2022-05-24 15:39:41 +0100

    free list function

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,10 +1,11 @@
+#include <stdlib.h>
 #include "lists.h"
 
 /**
- *  * free_list - frees a list
- *   * @head: head of the linked list.
- *    * Return: no return.
- *     */
+ * free_list - frees a list
+ * @head: head of the linked list.
+ * Return: no return.
+ */
 void free_list(list_t *head)
 {
 	list_t *current;
