45ded943785093241dd38e3109c947de6246a218
linda chukwu <lindachukwu961@gmail.com>
2022-05-24 15:50:58 +0100

    adding node to the beginiing of a list function

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -12,18 +12,17 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new;
-	size_t nchar;
+	unsigned int = 0;
 
-	new = malloc(sizeof(lists_t));
-	if (new == NULL)
+	while (str[len])
+		len++;
+
+	new = malloc(sizeof(list_t));
+	if (!new)
 		return (NULL);
 
 	new->str = strdup(str);
-
-	for (nchar = 0; str[nchar]; nchar++)
-		;
-
-	new->len = nchar;
+	new->len = len;
 	new->next = *head;
 	*head = new;
 
