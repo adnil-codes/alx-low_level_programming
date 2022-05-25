725b0c8c7aac2a5c8ee780c7bd18a71fc3a781eb
linda chukwu <lindachukwu961@gmail.com>
2022-05-25 11:43:44 +0100

    adds node to a list

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -12,17 +12,18 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new;
-	unsigned int = 0;
-
-	while (str[len])
-		len++;
+	size_t nchar;
 
 	new = malloc(sizeof(list_t));
-	if (!new)
+	if (new == NULL)
 		return (NULL);
 
 	new->str = strdup(str);
-	new->len = len;
+
+	for (nchar = 0; str[nchar]; nchar++)
+		;
+
+	new->len = nchar;
 	new->next = *head;
 	*head = new;
 
