84ec8459e9c4ffbc786220287e1741b9e706a711
linda chukwu <lindachukwu961@gmail.com>
2022-05-25 11:47:59 +0100

    prints in a 64bit

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,16 +1,15 @@
-section	.text
-   global_start   ;must be declared for linker (ld)
-   	
-_start:	          ;tells linker entry point
-   mov	edx,len   ;message length
-   mov	ecx,msg   ;message to write
-   mov	ebx,1     ;file descriptor (stdout)
-   mov	eax,4     ;system call number (sys_write)
-   int	0x80      ;call kernel
-		       	
-   mov	eax,1     ;system call number (sys_exit)
-   int	0x80      ;call kernel
+SECTION .data
+msg:	db "Hello, Holberton", 0
+fmt:	db "%s", 10, 0
 
-   section .data
-   msg db 'Hello, Holberton \n', 0xa  ;string to be printed
-   len equ $ - msg     ;length of the string~
+	SECTION .text
+	extern printf
+	global main
+main:
+	mov esi, msg
+	mov edi, fmt
+	mov eax, 0
+	call printf
+
+	mov eax, 0
+	ret
