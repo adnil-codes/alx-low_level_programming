e969455bbd1d09c13cd97d4b9c7e469f4fc6797f
linda chukwu <lindachukwu961@gmail.com>
2022-05-26 16:05:09 +0100

    returns the nth node of a linked list

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -11,9 +11,9 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int i;
 
-	for (; i < index && head != NULL; i++)
+	for (i = 0; i < index && head != NULL; i++)
 	{
-		head = head->next
+		head = head->next;
 	}
 	return (head);
 }
