2ea31a07951624949514642a9e0dbe7ce55c4f94
linda chukwu <lindachukwu961@gmail.com>
2022-06-01 16:40:15 +0100

    updates task

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -4,6 +4,7 @@
  * create_file - creates a file
  * @filename: file name
  * @text_content: content written in the file
+ *
  * Return: 1 if sucessful and -1 if test fails
  */
 int create_file(const char *filename, char *text_content)
@@ -15,7 +16,7 @@ int create_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 
-	fd = open(filename, O_CREAT | 0_WRONLY | O_TRUNC, 0600);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
 	if (fd == -1)
 		return (-1);
@@ -31,7 +32,7 @@ int create_file(const char *filename, char *text_content)
 	if (rwr == -1)
 		return (-1);
 
-	close (fd);
+	close(fd);
 
 	return (1);
 }
