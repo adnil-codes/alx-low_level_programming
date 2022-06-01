16cbcae876725b554bca1e0ba851ec48e6af2005
linda chukwu <lindachukwu961@gmail.com>
2022-06-01 16:35:40 +0100

    update

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -261,7 +261,6 @@ void close_elf(int elf)
 
 /**
  * main - Displays the information contained in the
- *        ELF header at the start of an ELF file.
  * @argc: The number of arguments supplied to the program.
  * @argv: An array of pointers to the arguments.
  *
